7833c398e886a53b6516b83a9b5f23928357f539
rensongliang <958769009@qq.com>
2018-03-07 17:01:16 +0800

    Update method name

--- src/main/java/com/unisys/training/dao/FlightDao.java
+++ src/main/java/com/unisys/training/dao/FlightDao.java
@@ -11,18 +11,18 @@ public interface FlightDao {
             "#{id}, #{airl_code}, #{flt_number}, #{schd_date_time}, #{arrvdept}, #{acfttype}," +
             "#{flgttype}, #{reno}, #{domsintl}, #{actual_date_time}, #{estimated_date_time}, " +
             "#{user_code}, #{terminal_code})")
-    public int CalendarInsert(Flight flight);
+    public int FlightInsert(Flight flight);
 
     @Delete("delete from flight where id = #{id} limit 1")
-    public int CalendarDelete(int id);
+    public int FlightDelete(int id);
 
     @Update("update flight set title=#{title}, importance = #{importance}, " +
             "finishDate = #{finishDate} where id = #{id} limit 1")
-    public int CalendarUpdate(Flight flight);
+    public int FlightUpdate(Flight flight);
 
     @Select("select * from flight where id = #{id} limit 1")
-    public Flight CalendarSelectOne(Flight flight);
+    public Flight FlightSelectOne(Flight flight);
 
     @Select("select * from flight order by id desc limit #{whichNum},10")
-    public List<Flight> CalendarSelectAll(int whichNum);
+    public List<Flight> FlightSelectAll(int whichNum);
 }
