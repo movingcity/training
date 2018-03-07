6de237a5d2e415e99591954f57e6af5f84138e51
hanxb66 <xuebin.han@unisys.com>
2018-03-07 18:36:39 +0800

    Added Route service implement

--- src/main/java/com/unisys/training/service/impl/RouteServiceImpl.java
+++ src/main/java/com/unisys/training/service/impl/RouteServiceImpl.java
@@ -1,7 +1,42 @@
 package com.unisys.training.service.impl;
 
+import com.unisys.training.dao.RouteDao;
+import com.unisys.training.po.Flight;
+import com.unisys.training.po.Route;
+import com.unisys.training.service.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class RouteServiceImpl {
+public class RouteServiceImpl implements RouteService{
+    @Autowired
+    private RouteDao routeDao;
+
+    @Override
+    public int RouteInsert(Route route) {
+        return routeDao.RouteInsert(route);
+    }
+
+    @Override
+    public int RouteDelete(int id) {
+        return routeDao.RouteDelete(id);
+    }
+
+    @Override
+    public int RouteUpdate(Route route) {
+        return routeDao.RouteUpdate(route);
+    }
+
+    @Override
+    public Route RouteSelectOne(Route route) {
+        return routeDao.RouteSelectOne(route);
+    }
+
+    @Override
+    public List<Route> RouteSelectByFlight(Flight flight) {
+                return routeDao.RouteSelectByFlight(flight);
+    }
+
 }
