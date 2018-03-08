7e398d85ded61d72b3da534100156cff9e87a308
rensongliang <958769009@qq.com>
2018-03-08 12:32:26 +0800

    Add logger to FlightController

--- src/main/java/com/unisys/training/controller/FlightController.java
+++ src/main/java/com/unisys/training/controller/FlightController.java
@@ -1,10 +1,10 @@
 package com.unisys.training.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.unisys.training.po.Flight;
 import com.unisys.training.service.FlightService;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @RestController
 public class FlightController {
@@ -21,7 +20,7 @@ public class FlightController {
 
     //Static date formatter
     private static final SimpleDateFormat date_formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-
+    private final Logger logger = LoggerFactory.getLogger(FlightController.class);
     @RequestMapping("/flight/insert")
     private Object flightInsert(HttpServletRequest req) {
         String airl_code = req.getParameter("airl_code");
@@ -119,12 +118,14 @@ public class FlightController {
         String id = req.getParameter("id");
         Flight flight = new Flight();
         flight.setId(Integer.parseInt(id));
+        logger.info(id);
         return JSONObject.toJSONString(flightService.FlightSelectOne(flight));
     }
 
     @RequestMapping("/flight/selectAll")
     private Object flightSelectAll(HttpServletRequest req) {
         String whichPage = req.getParameter("whichNum");
+        logger.info(whichPage);
         return JSONObject.toJSONString(flightService.FlightSelectAll(Integer.parseInt(whichPage)));
     }
 
