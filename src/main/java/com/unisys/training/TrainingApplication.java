cdcfcdcb46d4e943a09d6adbf77c62e05090db1c
rensongliang <958769009@qq.com>
2018-03-08 12:06:13 +0800

    Add DateSource to current application

--- src/main/java/com/unisys/training/TrainingApplication.java
+++ src/main/java/com/unisys/training/TrainingApplication.java
@@ -2,9 +2,30 @@ package com.unisys.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
 public class TrainingApplication {
+
+	@Bean(name = "dataSource")
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+          return DataSourceBuilder.create().type(com.alibaba.druid.pool.DruidDataSource.class).build();
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
--- src/main/java/com/unisys/training/controller/FlightController.java
+++ src/main/java/com/unisys/training/controller/FlightController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.unisys.training.po.Flight;
 import com.unisys.training.service.FlightService;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
