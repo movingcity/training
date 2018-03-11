ffd1f77aac519cf14a0540f150b789c86e58aab0
movingcity <movingcity@163.com>
2018-03-11 14:24:52 +0800

    列表界面更新

--- src/main/webapp/js/flightManage.js
+++ src/main/webapp/js/flightManage.js
@@ -1,74 +1,57 @@
 var tbodySelectAll;
 
+function queryParams(params) {
+    return params
+}
+
 function funFlightSelectAll() {
 
-    $.ajax({
-        type: "get",
-        url: "/flight/selectAll",
-        async: true,
-        timeout: 10000,
-        data: "whichNum=" + 1,
-        dataType: "json",
-        success: function (msg) {
-            console.log(msg);
-            $('#table').bootstrapTable({
-                columns: [{
-                    field: 'flt_number',
-                    title: '航班号'
-                }, {
-                    field: 'airl_code',
-                    title: '航空公司'
-                }, {
-                    field: 'schd_date_time',
-                    title: '计划时间'
-                }, {
-                    field: 'arrvdept',
-                    title: '到港/离港'
-                }, {
-                    field: 'domsintl',
-                    title: '国际/国内'
-                },
-                    {
-                        field: 'operate',
-                        title: '操作',
-                        align: 'center',
-                        events: operateEvents,
-                        formatter: operateFormatter
-                    }],
-                data: msg
-            });
-            // tbodySelectAll = "";
-            // for(var i = 0; i < msg.length; i++) {
-            // 	tbodySelectAll += "<tr class='info'>";
-            // 	tbodySelectAll += "<td>";
-            // 	tbodySelectAll += msg[i].flt_number == null ? "---" : msg[i].flt_number;
-            // 	tbodySelectAll += "</td>";
-            // 	tbodySelectAll += "<td>";
-            // 	tbodySelectAll += msg[i].airl_code == null ? "---" : msg[i].airl_code;
-            // 	tbodySelectAll += "</td>";
-            // 	tbodySelectAll += "<td>";
-            // 	tbodySelectAll += msg[i].schd_date_time == null ? "---" : msg[i].schd_date_time;
-            // 	tbodySelectAll += "</td>";
-            // 	tbodySelectAll += "<td>";
-            // 	tbodySelectAll += msg[i].arrvdept == "A" ? "到港" : "离港";
-            // 	tbodySelectAll += "</td>";
-            // 	tbodySelectAll += "<td>";
-            // 	tbodySelectAll += msg[i].domsintl == null ? "---" : msg[i].domsintl;
-            // 	tbodySelectAll += "</td>";
-            // 	tbodySelectAll += "<td>";
-            // 	tbodySelectAll += "<a href='javascript:void(0)' onclick=funFlightUpdate()>更新</a>";
-            // 	tbodySelectAll += "</td>";
-            // 	tbodySelectAll += "<td>";
-            // 	tbodySelectAll += "<a href='javascript:void(0)' onclick=funFlightDelete()>删除</a>";
-            // 	tbodySelectAll += "</td>";
-            // 	tbodySelectAll += "</tr>";
-            // 	$("#tbodySelectAll").html(tbodySelectAll);
-            // }
+    var url = '/flight/selectAll';
+    $('#table').bootstrapTable({
+        method: 'get',
+        url: url,
+        cache: false,
+        striped: true,
+        pagination: true,
+        pageList: [10, 20],
+        pageSize: 10,
+        pageNumber: 1,
+        search: true,
+        sidePagination: 'server',//设置为服务器端分页
+        queryParams: queryParams,//参数
+        showColumns: true,
+        showRefresh: true,
+        minimumCountColumns: 2,
+        clickToSelect: true,
+        smartDisplay: true,
+        formatNoMatches: function () {
+            return '无符合条件的记录';
         },
-        error: function (msg) {
-            console.log(msg);
-        }
+        columns: [{
+            field: 'flt_number',
+            title: '航班号'
+        }, {
+            field: 'airl_code',
+            title: '航空公司'
+        }, {
+            field: 'schd_date_time',
+            title: '计划时间'
+        }, {
+            field: 'arrvdept',
+            title: '到港/离港'
+        }, {
+            field: 'domsintl',
+            title: '国际/国内'
+        },
+            {
+                field: 'operate',
+                title: '操作',
+                align: 'center',
+                events: operateEvents,
+                formatter: operateFormatter
+            }]
     });
+
 }
 
 $(document).ready(function () {
@@ -77,7 +60,9 @@ $(document).ready(function () {
 
     $("#btnSelectAll").click(function () {
         funFlightSelectAll();
-
+    });
+    $(window).resize(function () {
+        $('#table').bootstrapTable('resetView');
     });
 })
 
@@ -95,23 +80,23 @@ function operateFormatter(value, row, index) {
 
 window.operateEvents = {
     'click .detail': function (e, value, row, index) {
-        window.location.href="FlightDetail.html?id="+ row.id;
+        window.location.href = "FlightDetail.html?id=" + row.id;
     },
     'click .remove': function (e, value, row, index) {
 
         $.ajax({
             type: "post",
-            url: "/flight/deleteById?id="+ row.id,
+            url: "/flight/deleteById?id=" + row.id,
             async: true,
             timeout: 10000,
-            data: "id="+ row.id,
-            success: function(msg) {
+            data: "id=" + row.id,
+            success: function (msg) {
                 console.log(msg);
-                if(msg = 1)
+                if (msg = 1)
                     window.alert("删除成功");
-                funFlightSelectAll();
+                $('#table').bootstrapTable('refresh');
             },
-            error: function(msg) {
+            error: function (msg) {
                 console.log(msg);
                 window.alert("删除失败" + msg.responseJSON.message);
             }
