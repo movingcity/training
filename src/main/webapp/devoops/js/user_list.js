50e3772b3448a379a6b09f5ebb5075b0344442f6
He Yuning <movingcityhe@gmail.com>
2018-03-15 17:54:06 +0800

    user list

--- src/main/webapp/devoops/js/user_list.js
+++ src/main/webapp/devoops/js/user_list.js
@@ -1,50 +1,5 @@
 var tbody;
 
-function queryParams(params) {
-    return params
-}
-
-
-function funUserSelectAll() {
-
-    var url = '/user/selectAll';
-    $('#table').bootstrapTable({
-        method: 'get',
-        cache: false,
-        striped: true,
-        pagination: true,
-        pageList: [10, 20],
-        pageSize: 10,
-        pageNumber: 1,
-        search: true,
-        sidePagination: 'server',//设置为服务器端分页
-        queryParams: queryParams,//参数
-        showColumns: true,
-        showRefresh: true,
-        minimumCountColumns: 2,
-        clickToSelect: true,
-        smartDisplay: true,
-        formatNoMatches: function () {
-            return '无符合条件的记录';
-        },
-        columns: [{
-            field: 'username',
-            title: '用户名'
-        }, {
-            field: 'locked',
-            title: '锁定？'
-        },
-            {
-                field: 'operate',
-                title: '操作',
-                align: 'center',
-                events: operateEvents,
-                formatter: operateFormatter
-            }]
-    });
-
-}
-
 $(document).ready(function () {
     $("#detail_div").attr("style", "display:none;");
     fillData();
@@ -56,13 +11,13 @@ $(document).ready(function () {
 function fillData() {
     tbody = "";
     var msg = [
-        {"id": "0","username": "user00", "locked": "0"},
-        {"id": "1","username": "user01", "locked": "0"},
-        {"id": "2","username": "user02", "locked": "1"},
-        {"id": "3","username": "user03", "locked": "1"},
-        {"id": "4","username": "user04", "locked": "0"},
-        {"id": "5","username": "user05", "locked": "0"}
-        ];
+        {"id": "0", "username": "user00", "locked": "0"},
+        {"id": "1", "username": "user01", "locked": "0"},
+        {"id": "2", "username": "user02", "locked": "1"},
+        {"id": "3", "username": "user03", "locked": "1"},
+        {"id": "4", "username": "user04", "locked": "0"},
+        {"id": "5", "username": "user05", "locked": "0"}
+    ];
     for (var i = 0; i < msg.length; i++) {
         tbody += "<tr>";
         tbody += "<td>";
@@ -75,7 +30,7 @@ function fillData() {
         tbody += msg[i].locked == 1 ? "是" : "否";
         tbody += "</td>";
         tbody += "<td>";
-        tbody += "<a href='javascript:void(0)' onclick=funUserUpdate(" + msg[i].id + ")>更新</ a>";
+        tbody += "<a href='javascript:void(0)' onclick=funUserUpdate(" + msg[i] + ")>更新</ a>";
         tbody += "</td>";
         tbody += "<td>";
         tbody += "<a href='javascript:void(0)' onclick=funUserDelete(" + msg[i].id + ")>删除</a>";
@@ -95,6 +50,27 @@ function funUserInsert() {
     $("#expand").click();
 }
 
-window.operateEvents = {
+function funUserDelete(id) {
+    var msg = "您真的确定要删除吗？" + id;
+    if (confirm(msg) === true) {
+        // 调用删除
+        $.ajax({
+            type: "post",
+            url: "/deleteUser",
+            async: true,
+            timeout: 10000,
+            data: "id=" + 1,
+            dataType: "json",
+            success: function (msg) {
+                console.log(msg);
+            },
+            error: function (msg) {
+                console.log(msg);
+            }
+        });
+    } else {
+        return false;
+    }
+}
 
-};
+window.operateEvents = {};
