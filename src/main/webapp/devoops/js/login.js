0b436cdd06d26a1f633aa560c782ad925bc275eb
He Yuning <movingcityhe@gmail.com>
2018-03-15 17:42:19 +0800

    加幻灯片

---
+++ src/main/webapp/devoops/js/login.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $("#aLogin").click(function () {
+        // login ajax
+        $.ajax({
+            type: "get",
+            url: "/login",
+            async: true,
+            timeout: 10000,
+            data: "username=" + $("#username").val() + "&password=" + $("#password").val(),
+            dataType: "json",
+            success: function (msg) {
+                console.log(msg);
+                if (msg !== 1) {
+                   alert("登录失败，请检查用户名和密码")
+                }
+                else
+                {
+                    //成功
+                    window.location.href="/bg/index.html";
+                    var lStorage = window.localStorage;
+                    lStorage.setItem("username",$("#username").val());
+                    alert("登录成功 " + $("#username").val());
+                }
+            },
+            error: function (msg) {
+                console.log(msg);
+
+                window.location.href="/bg/index.html";
+                var lStorage = window.localStorage;
+                lStorage.setItem("username",$("#username").val());
+                alert("登录成功 " + $("#username").val());
+            }
+        });
+
+    });
+})
